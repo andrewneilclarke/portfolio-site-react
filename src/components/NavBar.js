3db8dc36da741ca68e2112b15557ef685c654466
andrewneilclarke <andrewneilclarke@gmail.com>
2021-09-02 09:40:31 +0200

    fixed sliding menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,48 +4,30 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { useState } from 'react'
 
 const NavBar = () => {
+
     const [showMenu, setShowMenu] = useState(false)
-    const menuBtn = document.querySelector('.menu-btn');
-    const menu = document.querySelector('.menu');
-    const menuNav = document.querySelector('.menu-nav');
-    const menuBranding = document.querySelector('.menu-branding');
-    const navItems = document.querySelectorAll('.nav-item');
 
     const toggleMenu = () => {
-        if(!showMenu) {
-            menuBtn.classList.add('close');
-            menu.classList.add('show');
-            menuNav.classList.add('show');
-            menuBranding.classList.add('show');
-            navItems.forEach(item => item.classList.add('show'));
-            setShowMenu(true)
-        } else {
-            menuBtn.classList.remove('close');
-            menu.classList.remove('show');
-            menuNav.classList.remove('show');
-            menuBranding.classList.remove('show');
-            navItems.forEach(item => item.classList.remove('show'));
-            setShowMenu(false);
-        }
+        setShowMenu(!showMenu)
     }
 
     return (    
         <header>
-            <div className="menu-btn" onClick={toggleMenu}>
+            <div className={!showMenu ? "menu-btn close" : "menu-btn"} onClick={toggleMenu}>
                 <div className="btn-line"></div>
                 <div className="btn-line"></div>
                 <div className="btn-line"></div>
             </div>
-            <nav className="menu">
-                <div className="menu-branding">
+            <nav className={!showMenu ? "menu show" : "menu"}>
+                <div className={!showMenu ? "menu-branding show" : "menu-branding"}>
                     <div className="portrait"></div>
                 </div>
-                <ul className="menu-nav">
+                <ul className={!showMenu ? "menu-nav show" : "menu-nav"}>
                     <li className="nav-item">
-                        <a href="!#" className="nav-link" alt="about">About</a>
-                        <a href="!#" className="nav-link" alt="services">My Services</a>
-                        <a href="!#" className="nav-link" alt="projects">Projects</a>
-                        <a href="!#" className="nav-link" alt="contact">Hire / Contact me</a>
+                        <a href="!#" className={!showMenu ? "nav-link show" : "nav-link"} alt="about">About</a>
+                        <a href="!#" className={!showMenu ? "nav-link show" : "nav-link"} alt="services">My Services</a>
+                        <a href="!#" className={!showMenu ? "nav-link show" : "nav-link"} alt="projects">Projects</a>
+                        <a href="!#" className={!showMenu ? "nav-link show" : "nav-link"} alt="contact">Hire / Contact me</a>
                     </li>
                 </ul>
             </nav>
